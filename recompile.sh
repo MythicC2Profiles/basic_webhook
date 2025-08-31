930294f601b1b3dc8b1cdcb40f10dd912f164954
lsecqt <lsecqt@gmail.com>
2025-08-31 15:12:15 -0400

    Refactored recompile.sh for easier modification

--- recompile.sh
+++ recompile.sh
@@ -4,11 +4,15 @@ if [[ "$EUID" -ne 0 ]]; then
    exit 1
 fi
 
-WEBHOOKPATH="/home/kali/source/basic_webhook/C2_Profiles/webhook"  # Full path to Payload_Type/haunt, see my example
+# EDIT BELOW
+# EXAMPLE: /home/kali/source/basic_webhook/C2_Profiles/webhook
+WEBHOOKPATH="/path/to/C2_Profiles/webhook"  # Full path to Payload_Type/webhook, see my example
+# EXAMPLE: /opt/Mythic
+MYTHICPATH="/path/to/Mythic"
 
 cd "/opt/Mythic" || exit 1
-sudo /opt/Mythic/mythic-cli remove basic_webhook
-sudo rm -rf /opt/Mythic/InstalledServices/basic_webhook
-sudo cp -r "$WEBHOOKPATH" /opt/Mythic/InstalledServices
-sudo ./mythic-cli add webhook
-sudo ./mythic-cli build webhook
+sudo "$MYTHICPATH"/mythic-cli remove webhook
+sudo rm -rf "$MYTHICPATH"/InstalledServices/webhook
+sudo cp -r "$WEBHOOKPATH" "$MYTHICPATH"/InstalledServices
+sudo "$MYTHICPATH"/mythic-cli add webhook
+sudo "$MYTHICPATH"/mythic-cli build webhook
